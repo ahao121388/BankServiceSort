1886dcdfb353d1050425d33abb926685c2e6e0ec
ahao121388 <mark960907@163.com>
2022-02-15 21:55:33 +0800

    Initial commit

---
+++ src/main/java/com/chao/bankservicesort/BankServiceSortApplication.java
@@ -0,0 +1,13 @@
+package com.chao.bankservicesort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankServiceSortApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankServiceSortApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chao/bankservicesort/BankServiceSortApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chao.bankservicesort;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankServiceSortApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
