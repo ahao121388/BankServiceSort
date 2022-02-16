24a15ebd710df8cf5ceb2a7d45299485a8b50388
ahao121388 <mark960907@163.com>
2022-02-16 12:46:52 +0800

    新增功能--登录2

--- src/main/java/com/chao/bankservicesort/sortservice/controller/SortController.java
+++ src/main/java/com/chao/bankservicesort/sortservice/controller/SortController.java
@@ -1,13 +1,10 @@
 package com.chao.bankservicesort.sortservice.controller;
 
-
 import com.chao.bankservicesort.sortservice.dto.SetterLoginDto;
 import com.chao.bankservicesort.sortservice.service.SortService;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-
 import javax.annotation.Resource;
 
 @CrossOrigin
